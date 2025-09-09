abbbdaf366fbac7fe6c071177d715491d32140fc
Amir Rezaali <132251387+amirrezaali1379@users.noreply.github.com>
2025-09-09 21:13:59 +0330

    shokoh invites

---
+++ invite/link_maker.py
@@ -0,0 +1,16 @@
+with open('names.txt','r',encoding='utf8') as file:
+    names=file.readlines()
+
+
+with open('index.html','r',encoding='utf8') as file:
+    html=file.read()
+
+id='1'
+
+for i,name in enumerate(names):
+    html2=html.replace(' namemamad ',name)
+    with open(f'{id}/{i}.html','w',encoding='utf8') as file:
+        file.write(html2)
+    with open(f'1_links.txt','a',encoding='utf8') as file:
+        file.write(f'{(name.strip())}:   https://amirrezaali1379.github.io/invite/{id}/{i}.html')
+        file.write('\n')
