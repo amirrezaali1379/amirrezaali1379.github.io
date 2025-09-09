11656e93f3154df7e2c677af639b9fae3048a9f6
Amir Rezaali <132251387+amirrezaali1379@users.noreply.github.com>
2025-09-09 21:18:07 +0330

    .

--- invite/link_maker.py
+++ invite/link_maker.py
@@ -1,16 +1,16 @@
-with open('names.txt','r',encoding='utf8') as file:
+with open('invite/names.txt','r',encoding='utf8') as file:
     names=file.readlines()
 
 
-with open('index.html','r',encoding='utf8') as file:
+with open('invite/index.html','r',encoding='utf8') as file:
     html=file.read()
 
 id='1'
 
 for i,name in enumerate(names):
     html2=html.replace(' namemamad ',name)
-    with open(f'{id}/{i}.html','w',encoding='utf8') as file:
+    with open(f'invite/{id}/{i}.html','w',encoding='utf8') as file:
         file.write(html2)
-    with open(f'1_links.txt','a',encoding='utf8') as file:
+    with open(f'invite/{id}_links.txt','a',encoding='utf8') as file:
         file.write(f'{(name.strip())}:   https://amirrezaali1379.github.io/invite/{id}/{i}.html')
         file.write('\n')
