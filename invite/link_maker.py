24c08ebe0938502b01530f0063e0295ca54269f7
Amir Rezaali <132251387+amirrezaali1379@users.noreply.github.com>
2025-09-09 21:31:25 +0330

    moshtarak2

--- invite/link_maker.py
+++ invite/link_maker.py
@@ -5,7 +5,7 @@ with open('invite/names.txt','r',encoding='utf8') as file:
 with open('invite/index.html','r',encoding='utf8') as file:
     html=file.read()
 
-id='4'
+id='5'
 
 for i,name in enumerate(names):
     html2=html.replace(' namemamad ',name)
