027e31b19790abbbe7e2ed403244acd04c8f31cc
Amir Rezaali <132251387+amirrezaali1379@users.noreply.github.com>
2025-09-10 09:03:12 +0330

    marhoome changed

---
+++ invite/html_editor.py
@@ -0,0 +1,18 @@
+def change_text(current_text,new_text,file_name,id):
+    with open(f'invite/{id}/{file_name}','r',encoding='utf8') as file:
+        html=file.read()
+        
+    html2=html.replace(current_text,new_text)
+
+    with open(f'invite/{id}/{file_name}','w',encoding='utf8') as file:
+        file.write(html2)
+
+import os
+id='16'
+folder_path = f"invite/{id}"  # مسیر پوشه‌ات
+
+files = os.listdir(folder_path)
+
+for f in files:
+    if os.path.isfile(os.path.join(folder_path, f)):
+        change_text('مرحومه','مرحومه مغفوره حاجیه خانم',f,id)
